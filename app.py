145a423070ba179e74f9698941abfcfa44f5e5fb
rebeccaSimmonds19 <rmsimmonds1919@gmail.com>
2017-11-09 17:32:59 +0000

    changed for arguments to app

--- app.py
+++ app.py
@@ -1,40 +1,37 @@
 import psycopg2
 import argparse
 
-#main function
-def main(self):
-    parse(self)
+class DatabaseLoader():
+    #main function
+    def __init__(self):
+        parser = argparse.ArgumentParser(description='postgresql database')
+        parser.add_argument('--server', help='the postgreql ip address')
+        parser.add_argument('--user', help='username of postgresql user')
+        parser.add_argument('--password', help='user password for postgresql')
+        parser.add_argument('--dbname',help='the name of the database')
+        args = parser.parse_args()
+        self.setupDb(args.server, args.user, args.password, args.dbname)
 
-# parsing commandline args
-def parse(self):
-    parser = argparse.ArgumentParser(description='postgresql database')
-    parser.add_argument('--server', help='the postgreql ip address')
-    parser.add_argument('--user', help='username of postgresql user')
-    parser.add_argument('--password', help='user password for postgresql')
-    parser.add_argument('--dbname',help='the name of the database')
-    args = parser.parse_args()
-    self.setupDb(args.server)
+    #takes the csv and inserts it into the db
+    def setupDb(self, server, user, password, dbname):
+        conn = psycopg2.connect("host=" + server + " port='5432' dbname="+dbname+" user="+user+" password="+password)
+        cur = conn.cursor()
 
-#takes the csv and inserts it into the db
-def setupDb(self, server, user, password, dbname):
-    conn = psycopg2.connect("host=" + server + " port='5432' dbname="+dbname+" user="+user+" password="+password)
-    cur = conn.cursor()
-
-    # does table exist
-    tb_exists = "select exists(select relname from pg_class where relname='" + "wine_reviews" + "')"
-    cur.execute(tb_exists)
-    execute = cur.fetchone()[0]
-    if not execute:
-        # make table
-        cur.execute(
-            'create table wine_reviews(country VARCHAR, designation VARCHAR, points INT, price VARCHAR, province VARCHAR, region_1 VARCHAR, region_2 VARCHAR, variety VARCHAR, winery VARCHAR);')
+        # does table exist
+        tb_exists = "select exists(select relname from pg_class where relname='" + "wine_reviews" + "')"
+        cur.execute(tb_exists)
+        execute = cur.fetchone()[0]
+        if not execute:
+            # make table
+            cur.execute(
+                'create table wine_reviews(country VARCHAR, designation VARCHAR, points INT, price VARCHAR, province VARCHAR, region_1 VARCHAR, region_2 VARCHAR, variety VARCHAR, winery VARCHAR);')
+            conn.commit()
+        # copy csv
+        f = open(r'/opt/app-root/src/wineData.csv', 'r')
+        cur.copy_from(f, "wine_reviews", sep=',')
         conn.commit()
-    # copy csv
-    f = open(r'/opt/app-root/src/wineData.csv', 'r')
-    cur.copy_from(f, "wine_reviews", sep=',')
-    conn.commit()
-    f.close()
+        f.close()
 
 
 if __name__ == '__main__':
-    main()
+    DatabaseLoader()
