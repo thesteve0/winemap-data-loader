f24c0f57584cef2167bd10c57fe05b770103985d
rebeccaSimmonds19 <rmsimmonds1919@gmail.com>
2017-11-13 13:23:03 +0000

    typo

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ class DatabaseLoader():
 
     #takes the csv and inserts it into the db
     def setupDb(self, server, user, password, dbname):
+        print(server)
         conn = psycopg2.connect("host=" + str(server) + " port='5432' dbname=" + str(dbname) + " user=" + str(user) + " password=" + str(password))
         cur = conn.cursor()
 
